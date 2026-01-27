85452879ca1bc7a9cde03d3ff74795a413d450b6
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-01-28 00:01:52 +0545

    Implement Class Practice Task with student details
    This script includes three tasks demonstrating variable assignment, user input, and formatted string output to display student details and marks.

---
+++ Python-Learning-Journey/basics/Class_Practice_Task.py
@@ -0,0 +1,32 @@
+#Task 1
+#in this task we have assigned variables and end,sep is used
+Student_name = "Asawari"
+Student_rollNo = 41
+Student_adhaarNo = 123456
+Student_dept =  "AIDS"
+print("My name is", Student_name,end=" ", sep=" ")
+print("from",Student_dept,"department",end=" ", sep=" ")
+print("having roll no",Student_rollNo,end=" ", sep=" ")
+print("and having addhar no",Student_adhaarNo,end=" ", sep=" ")
+
+#Task 2
+# in this task f-string concept is used
+Student_name = input("Enter your name: ") #this take input name from the user
+Student_rollNo = int(input("Enter roll no: ")) #this take input roll no
+Student_adharNo = int(input("Enter aadhar no: ")) #this take input aadhar
+Student_dept = input("Enter dept: ") #this take input dept
+print(f"My name is {Student_name} from {Student_dept} department having roll no {Student_rollNo} and Aadhar no {Student_adharNo}")
+
+#Task 3
+Student_name = input("Enter your name: ")  # this take input name from the user
+Student_rollNo = int(input("Enter roll no: "))  # this take input roll no
+Student_adharNo = int(input("Enter aadhar no: "))  # this take input aadhar
+Student_dept = input("Enter dept: ")  # this take input dept
+
+Marks_oF_OS = int(input("Enter OS marks: "))  # this takes input marks of 4 subjects
+Marks_oF_PYTHON = int(input("Enter Python marks: "))
+Marks_oF_DATA_ANALYSIS = int(input("Enter Data Analysis marks: "))
+Marks_oF_DM = int(input("Enter DM marks: "))
+
+print(f"My name is {Student_name} from {Student_dept} department having roll no {Student_rollNo} and Aadhar no {Student_adharNo}. Marks are: OS={Marks_oF_OS}, Python={Marks_oF_PYTHON}, Data Analysis={Marks_oF_DATA_ANALYSIS}, DM={Marks_oF_DM}.")
+# using f string output will printed in 1 single line
