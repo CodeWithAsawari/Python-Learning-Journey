3f5df81ffce98e7c9b822fddd97d7b21a961d82e
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-01-27 23:15:21 +0545

    Add typecasting examples in typecasting.py
    Demonstrate various typecasting methods in Python including string to int, int to float, float to int, int to string, string to float, and boolean conversion.

---
+++ Python-Learning-Journey/basics/typecasting.py
@@ -0,0 +1,30 @@
+ # String to Int
+a = "10"
+a = int(a)
+print(a)
+
+# Int to Float
+b = 5
+b = float(b)
+print(b)
+
+# Float to Int
+c = 7.9
+c = int(c)
+print(c)
+
+# Int to String
+d = 100
+d = str(d)
+print(d)
+
+# String to Float
+e = "25.5"
+e = float(e)
+print(e)
+
+# To Boolean
+f = 0
+g = 1
+print(bool(f))
+print(bool(g))
