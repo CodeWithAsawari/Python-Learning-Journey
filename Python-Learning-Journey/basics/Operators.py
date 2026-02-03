c1520becdf43503d662d7e2de3e3d119cef020c9
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-03 21:34:53 +0545

    new file added

---
+++ Python-Learning-Journey/basics/Operators.py
@@ -0,0 +1,49 @@
+# Arithmetic Operators
+a = 4
+b = 5
+print(a+b)  # addition
+print(a-b)  # subtraction
+print(a*b)  # Multiplication
+print(a/b)  # division
+print(a//b) # floor division
+print(a%b)  # Modulus
+print(a**b) # Exponentiation
+
+# Assignment Operators
+a = 5  # = operator
+print(a)
+a=3
+a += 5  # += operator
+print(a)
+a-=5  # -= operator
+print(a)
+a*=5 # *= operator
+print(a)
+x = 5
+x /= 3 # /= operator
+print(x)
+x = 5
+x%=3 # %= operator
+print(x)
+x = 5
+x//=3 # //= operator
+print(x)
+x = 5
+x **= 3 # **= operator
+print(x)
+x = 5
+x &= 3 # &= operator
+print(x)
+x = 5
+x |= 3 # |= operator
+print(x)
+x = 5
+x ^= 3  # ^= operator
+print(x)
+x = 5
+x >>= 3 # >>= operator
+print(x)
+x = 5
+x <<= 3 # <<= operator
+print(x)
+print(x := 3) # := operator
