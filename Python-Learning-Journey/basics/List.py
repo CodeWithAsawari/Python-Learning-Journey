ad9698e36f4c573a9e5e9e722be2b987419e0569
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-04 20:45:49 +0545

    list concept added

---
+++ Python-Learning-Journey/basics/List.py
@@ -0,0 +1,15 @@
+#Create a List:
+a = ["Asawari","Sanjay"]
+print(a)
+# List items are ordered, changeable, and allow duplicate values.
+
+# List Length
+#To determine how many items a list has, use the len() function:
+a = ["Asawari","Sanjay" ]
+print(len(a))
+
+#The list() Constructor
+#It is also possible to use the list() constructor when creating a new list.
+a = list(("Asawari","Sanjay" ))
+print(a)
+
