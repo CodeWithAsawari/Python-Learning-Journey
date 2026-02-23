7fa00904cbccbba65d3d4555b52672c9bc58bfe1
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-23 21:46:14 +0545

    New concepts added

--- Python-Learning-Journey/Python Modules/Scipy.py
+++ Python-Learning-Journey/Python Modules/Scipy.py
@@ -172,3 +172,18 @@ from scipy import constants
 
 print(constants.hectare) #10000.0
 print(constants.acre)    #4046.856422399999
+
+"""Volume:
+Return the specified unit in cubic meters (e.g. liter returns 0.001)"""
+from scipy import constants
+
+print(constants.liter)            #0.001
+print(constants.litre)            #0.001
+print(constants.gallon)           #0.0037854117839999997
+print(constants.gallon_US)        #0.0037854117839999997
+print(constants.gallon_imp)       #0.00454609
+print(constants.fluid_ounce)      #2.9573529562499998e-05
+print(constants.fluid_ounce_US)   #2.9573529562499998e-05
+print(constants.fluid_ounce_imp)  #2.84130625e-05
+print(constants.barrel)           #0.15898729492799998
+print(constants.bbl)              #0.15898729492799998
