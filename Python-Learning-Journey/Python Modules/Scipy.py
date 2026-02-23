d4323f73be6690dfb7189cd04a83af10668f8cb4
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-23 21:29:25 +0545

    New concepts added

---
+++ Python-Learning-Journey/Python Modules/Scipy.py
@@ -0,0 +1,32 @@
+"""What is SciPy?
+SciPy is a scientific computation library that uses NumPy underneath.
+
+SciPy stands for Scientific Python.
+
+It provides more utility functions for optimization, stats and signal processing.
+
+Like NumPy, SciPy is open source so we can use it freely.
+
+SciPy was created by NumPy's creator Travis Olliphant.
+
+
+Why Use SciPy?
+If SciPy uses NumPy underneath, why can we not just use NumPy?
+
+SciPy has optimized and added functions that are frequently used in NumPy and Data Science.
+
+
+Which Language is SciPy Written in?
+SciPy is predominantly written in Python, but a few segments are written in C.
+
+import SciPy
+Once SciPy is installed, import the SciPy module(s) you want to use in your applications by adding the from scipy import module statement:
+
+
+
+"""
+"""Checking SciPy Version
+The version string is stored under the __version__ attribute."""
+import scipy
+print(scipy.__version__)
+
