13265bc5c40b13e5f75f16718e4ed000b459e46b
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-23 21:47:11 +0545

    New concepts added

--- Python-Learning-Journey/Python Modules/Scipy.py
+++ Python-Learning-Journey/Python Modules/Scipy.py
@@ -187,3 +187,13 @@ print(constants.fluid_ounce_US)   #2.9573529562499998e-05
 print(constants.fluid_ounce_imp)  #2.84130625e-05
 print(constants.barrel)           #0.15898729492799998
 print(constants.bbl)              #0.15898729492799998
+
+"""Speed:
+Return the specified unit in meters per second (e.g. speed_of_sound returns 340.5)"""
+from scipy import constants
+
+print(constants.kmh)            #0.2777777777777778
+print(constants.mph)            #0.44703999999999994
+print(constants.mach)           #340.5
+print(constants.speed_of_sound) #340.5
+print(constants.knot)           #0.5144444444444445
