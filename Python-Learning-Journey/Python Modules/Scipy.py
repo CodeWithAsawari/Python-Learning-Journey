2137c41c1b329a4049146a28192722ead2ca3c0e
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-23 21:50:01 +0545

    New concepts added

--- Python-Learning-Journey/Python Modules/Scipy.py
+++ Python-Learning-Journey/Python Modules/Scipy.py
@@ -205,3 +205,17 @@ from scipy import constants
 print(constants.zero_Celsius)      #273.15
 print(constants.degree_Fahrenheit) #0.5555555555555556
 
+"""Energy:
+Return the specified unit in joules (e.g. calorie returns 4.184)"""
+from scipy import constants
+
+print(constants.eV)            #1.6021766208e-19
+print(constants.electron_volt) #1.6021766208e-19
+print(constants.calorie)       #4.184
+print(constants.calorie_th)    #4.184
+print(constants.calorie_IT)    #4.1868
+print(constants.erg)           #1e-07
+print(constants.Btu)           #1055.05585262
+print(constants.Btu_IT)        #1055.05585262
+print(constants.Btu_th)        #1054.3502644888888
+print(constants.ton_TNT)       #4184000000.0
